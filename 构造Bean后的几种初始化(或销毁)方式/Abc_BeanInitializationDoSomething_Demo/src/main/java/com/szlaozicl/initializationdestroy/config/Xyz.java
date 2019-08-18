78b12c86ffe461074b56292dead84531c54ea672
JustryDeng <13548417409@163.com>
2019-08-18 19:15:40 +0800

    构造Bean后的几种初始化(或销毁)方式

--- 构造Bean后的几种初始化(或销毁)方式/Abc_BeanInitializationDoSomething_Demo/src/main/java/com/szlaozicl/initializationdestroy/config/Xyz.java
+++ 构造Bean后的几种初始化(或销毁)方式/Abc_BeanInitializationDoSomething_Demo/src/main/java/com/szlaozicl/initializationdestroy/config/Xyz.java
@@ -15,7 +15,7 @@ public class Xyz {
     /**
      * 注:在这里通过@Bean将Abc的声明周期交由Spring进行管理，同时指定其初始化方法、销毁方法。
      *
-     * 注:如果注入Abc时，采用的是懒加载，那么只有当程序第一次用到Abc时，才会走其构造以及后面紧接着的相关初始化方法。
+     * 注:如果注入Abc时，采用的是懒加载@Lazy，那么只有当程序第一次用到Abc时，才会走其构造以及后面紧接着的相关初始化方法。
      */
     @Bean(initMethod = "myInit", destroyMethod = "myDestroy")
     public Abc abc() {
